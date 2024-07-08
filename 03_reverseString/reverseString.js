c9bd45d850376145f7f66822f21187e3aefd483a
Aditi Misra <f20212737@pilani.bits-pilani.ac.in>
2024-07-09 00:21:48 +0800

    completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = '';
+    let count = string.length - 1;
+    for(let i = count; i >= 0; i--){
+        result = result.concat(string.at(i));
+        
+    }
+    return result;
 };
 
 // Do not edit below this line
