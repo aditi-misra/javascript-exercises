814b4b8191bb63cb62d9b25d057af379ea97d81a
Aditi Misra <f20212737@pilani.bits-pilani.ac.in>
2024-07-09 20:37:40 +0800

    completed exercise 6

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const tempInF = temp;
+
+  let tempInC = (tempInF - 32)*(5/9);
+  if(Number.isInteger(tempInC)){
+    return tempInC;
+  } else{
+    let rounded = tempInC.toFixed(1);
+    return parseFloat(rounded); 
+  }
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const tempInC = temp;
+
+  const tempInF = (tempInC*9/5)+32;
+  if(Number.isInteger(tempInF)){
+    return tempInF;
+  } else{
+    let rounded = tempInF.toFixed(1);
+    return parseFloat(rounded); 
+  }
+    
 };
 
 // Do not edit below this line
