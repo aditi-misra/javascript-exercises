f68d637f4d43a40df9d5ffd984d5ae6b80720044
Aditi Misra <f20212737@pilani.bits-pilani.ac.in>
2024-07-18 13:18:42 +0800

    completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //first we create a string with all valid characters ie all alphanumerical lowercase characters
+    const valid = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    
+    //now to create the valid string
+    const newString = string
+        .toLowerCase()
+        .split('')
+        .filter((character) => valid.includes(character))
+        .join('');
 
+    const reverse = newString.split('').reverse().join('');
+
+    return reverse === newString;
 };
 
 // Do not edit below this line
